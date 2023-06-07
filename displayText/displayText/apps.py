9dc6711db159aaee67df88e8bccbe04317009340
Ahmad_bankconnect <101721967+mzee2022@users.noreply.github.com>
2023-06-07 19:35:41 +0500

    Display hello world

---
+++ displayText/displayText/apps.py
@@ -0,0 +1,8 @@
+
+from django.apps import AppConfig
+AppConfig.default = True
+
+class DisplayTextConfig(AppConfig):
+#     default = True
+    name = "displayText.displayText"
+    verbose_name = "Hello World"
